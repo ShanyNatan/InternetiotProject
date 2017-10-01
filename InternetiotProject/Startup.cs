4998544ae010322c2b5da733b7481b50f2a4217f
Shany Natan <Shani20100@gmail..com>
2017-10-01 22:28:35 +0300

    Add project files.

---
+++ InternetiotProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternetiotProject.Startup))]
+namespace InternetiotProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
